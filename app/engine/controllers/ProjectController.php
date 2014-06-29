a49e15839379ab7f29d41eee7d8d6f4f95274722
Bastien Henry <bastien.henry@supinternet.fr>
2014-06-29 15:34:56 +0200

    add startline et deadline for create project

--- app/engine/controllers/ProjectController.php
+++ app/engine/controllers/ProjectController.php
@@ -6,18 +6,18 @@ use Walrus\core\WalrusController;
 use Walrus\core\WalrusForm;
 
 /**
- * Class ProjectController
- * @package engine\controllers
- */
+* Class ProjectController
+* @package engine\controllers
+*/
 class ProjectController extends WalrusController
 {
     public function index()
     {
         $res = $this->model('project')->index();
         // if empty($res) {
-        //     $this->register('message', 'no project found');
+        // $this->register('message', 'no project found');
         // } else {
-        //     $this->register('message', 'All projects :');
+        // $this->register('message', 'All projects :');
         // }
 
         // var_dump($res);
@@ -30,17 +30,17 @@ class ProjectController extends WalrusController
 
     public function create()
     {
-    	$form = new WalrusForm('form_project_create');
-    	// $form->check();
-    	if(!empty($_POST))
-    	{
-    		$this->model('project')->create();
+        $form = new WalrusForm('form_project_create');
+        // $form->check();
+        if(!empty($_POST))
+        {
+        $this->model('project')->create();
             return $this->index();
-    	}
+        }
 
-    	echo $form->render();
+        echo $form->render();
 
-    	$this->setView('create');
+        $this->setView('create');
     }
 
     public function edit($id)
--- app/engine/models/Project.php
+++ app/engine/models/Project.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\engine\models;
 
 use R;
@@ -7,15 +8,17 @@ class Project
 {
     public function create()
     {
-		$project = R::dispense('projects');
+        $project = R::dispense('projects');
 
-		$project->name = $_POST['name'];
-		$project->description = $_POST['description'];
+        $project->name = $_POST['name'];
+        $project->startline = $_POST['startline'];
+        $project->deadline = $_POST['deadline'];
+        $project->description = $_POST['description'];
 
-		R::store($project);
+        R::store($project);
 
-		return true;
-	}
+        return true;
+    }
 
     public function find($id)
     {
@@ -52,7 +55,7 @@ class Project
         return $project;
     }
 
-	public function delete($id)
+    public function delete($id)
     {
         $project = R::load('projects', $id);
 
@@ -63,7 +66,7 @@ class Project
 
         // if($_SESSION['user']['id'] != $project->getProperties()['users_id'])
         // {
-        //     return array('user.forbidden' => 'Vous n\'avez pas les droits de faire ca');
+        // return array('user.forbidden' => 'Vous n\'avez pas les droits de faire ca');
         // }
 
         R::trash($project);
