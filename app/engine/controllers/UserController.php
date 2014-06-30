c4d7e806353dd3ca486a722a277490e7f97e9308
Bastien Henry <bastien.henry@supinternet.fr>
2014-06-30 11:58:47 +0200

    front

--- app/engine/controllers/UserController.php
+++ app/engine/controllers/UserController.php
@@ -49,6 +49,8 @@ class UserController extends WalrusController
                 else
                 {
                  var_dump('else');
+                 var_dump($_SESSION);
+                 die('hello');
                     $this->go('/CleverManagement/');
                 }
             }
