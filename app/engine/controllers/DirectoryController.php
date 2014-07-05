7bec0ffd6e53769ff3c146ceac8f7e57a86381aa
FaustineLarmagna <f.larmagna@gmail.com>
2014-07-05 19:50:05 +0200

    started directories

---
+++ app/engine/controllers/DirectoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\engine\controllers;
+
+use Walrus\core\WalrusController;
+use Walrus\core\WalrusForm;
+
+/**
+ * Class DirectoryController
+ * @package engine\controllers
+ */
+class DirectoryController extends WalrusController
+{
+	public function index()
+	{
+
+	}
+
+	public function show($id)
+	{
+		
+	}
+
+	public function create()
+	{
+		
+	}
+
+	public function edit($id)
+	{
+		
+	}
+
+	public function delete($id)
+	{
+		
+	}
+}
---
+++ app/engine/models/Directory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\engine\models;
+
+use R;
+use app\engine\models\Common;
+
+class Directory extends Common
+{
+	public function index()
+	{
+
+	}
+
+	public function show($id)
+	{
+		
+	}
+
+	public function create()
+	{
+		
+	}
+
+	public function edit($id)
+	{
+		
+	}
+
+	public function delete($id)
+	{
+		
+	}
+}
